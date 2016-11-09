1d9b7b675129b6fae5c3f9e5eaf600349035ebbb
Sarang Joshi <sarangj@msn.com>
2016-11-09 03:33:49 -0800

    Added minor todo

--- android/UWCalendar/app/src/main/java/com/sarangjoshi/uwcalendar/HomeActivity.java
+++ android/UWCalendar/app/src/main/java/com/sarangjoshi/uwcalendar/HomeActivity.java
@@ -112,6 +112,7 @@ public class HomeActivity extends AppCompatActivity
                         }).setNegativeButton("Decline", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
+                                // TODO Decline
                             }
                         });
 

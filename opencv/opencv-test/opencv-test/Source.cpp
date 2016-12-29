cbf1e8054e3d905e27a4193cb7cfcbe5b0d4128a
Sarang Joshi <sarangj@msn.com>
2016-12-29 01:10:36 -0600

    Added period recognition

--- opencv/opencv-test/opencv-test/Source.cpp
+++ opencv/opencv-test/opencv-test/Source.cpp
@@ -3,7 +3,8 @@
 #include "opencv2/imgproc/imgproc.hpp"
 
 #include <iostream>
-#include <queue>
+#include <unordered_map>
+#include <functional>
 
 using namespace cv;
 using namespace std;
@@ -55,7 +56,7 @@ Vec4i findTopEdge(vector<Vec4i> lines, Vec4i leftEdge) {
 	Vec4i topEdge = { -1, -1, -1, -1 };
 
 	// Find the longest line that shares a point with the left edge
-	for (size_t i = 0; i < lines.size(); i++) {
+	for (uint32_t i = 0; i < lines.size(); i++) {
 		Vec4i l = lines[i];
 		// Horizontal?
 		if (IN_THRESH(l[1], l[3])) {
@@ -77,13 +78,7 @@ Vec4i findTopEdge(vector<Vec4i> lines, Vec4i leftEdge) {
 	return topEdge;
 }
 
-uint32_t getHourHeight(Mat timeline) {
-	Mat edges;
-	Canny(timeline, edges, 50, 200);
-	vector<vector<Point>> contours;
-	findContours(edges, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
-
-	// - Go through each of the contours and convert them into high and low Y values
+vector<Vec2i> contoursToRanges(vector<vector<Point>> contours) {
 	vector<Vec2i> ranges;
 	for (vector<Point> contour : contours) {
 		std::sort(contour.begin(), contour.end(), [](Point a, Point b) {
@@ -92,6 +87,17 @@ uint32_t getHourHeight(Mat timeline) {
 		Vec2i range = { contour.front().y, contour.back().y };
 		ranges.push_back(range);
 	}
+	return ranges;
+}
+
+uint32_t getHourHeight(Mat timeline) {
+	Mat edges;
+	Canny(timeline, edges, 50, 200);
+	vector<vector<Point>> contours;
+	findContours(edges, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
+
+	// - Go through each of the contours and convert them into high and low Y values
+	vector<Vec2i> ranges = contoursToRanges(contours);
 
 	std::sort(ranges.begin(), ranges.end(), [](Vec2i a, Vec2i b) {
 		if (a[0] == b[0]) {
@@ -104,7 +110,7 @@ uint32_t getHourHeight(Mat timeline) {
 	Mat rangesMat = Mat::zeros(edges.size(), CV_8UC3);
 	vector<Vec2i> combinedRanges;
 	Vec2i combinedRange = ranges[0];
-	for (size_t i = 1; i < ranges.size(); i++) {
+	for (uint32_t i = 1; i < ranges.size(); i++) {
 		Vec2i range = ranges[i];
 		// If the range is already contained, no extension needed
 		if (range[1] < combinedRange[1]) {
@@ -134,21 +140,122 @@ uint32_t getHourHeight(Mat timeline) {
 	combinedRanges.push_back(combinedRange);
 	drawLine(rangesMat, Vec4i(50, combinedRange[0], 50, combinedRange[1]), Scalar(255, 255, 255));
 
-	imwrite("../data/timelineRanges.jpg", rangesMat);
-
 	// - Finally, average out the distances between consecutive contours (top value to top value) and use as hour height
 	
 	// Ignore the zeroeth range because that's the one cut in half
 	uint32_t hourHeightSum = 0;
-	for (size_t i = 1; i < combinedRanges.size() - 1; i++) {
+	for (uint32_t i = 1; i < combinedRanges.size() - 1; i++) {
 		hourHeightSum += (combinedRanges[i + 1] - combinedRanges[i])[0];
 	}
 
 	return hourHeightSum / (combinedRanges.size() - 2);
 }
 
+typedef struct {
+	Vec2i range; // inclusive, exclusive
+	Vec3b pixel;
+} PixelRange;
+
+void processDay2(Mat daySchedule, uint32_t dayIndex, uint32_t hourHeight) {
+	Mat dsGray = daySchedule;
+	cvtColor(daySchedule, dsGray, CV_BGR2GRAY);
+
+	Mat thresh;
+	threshold(dsGray, thresh, 240, 255, THRESH_BINARY_INV);
+	rectangle(thresh, Point(0, 0), Point(thresh.cols / 10, thresh.rows), Scalar(0, 0, 0), CV_FILLED);
+	rectangle(thresh, Point((int)(thresh.cols * 0.9), 0), Point(thresh.cols, thresh.rows), Scalar(0, 0, 0), CV_FILLED);
+	imwrite("../data/thresh" + to_string(dayIndex) + ".jpg", thresh);
+
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+	findContours(thresh, contours, hierarchy, CV_RETR_EXTERNAL, CHAIN_APPROX_NONE);
+	vector<Vec2i> ranges = contoursToRanges(contours);
+	
+	Mat contoursMat = Mat::zeros(thresh.size(), CV_8UC3);
+	for (uint32_t i = 0; i < contours.size(); i++)
+	{
+		// If range is big enough, draw it
+		Vec2i range = ranges[i];
+		if (range[1] - range[0] > hourHeight / 6) {
+			Scalar color = Scalar(0, 255, 0);
+			drawContours(contoursMat, contours, i, color, 10, 8, hierarchy, 0);
+		}
+	}
+	imwrite("../data/contour" + to_string(dayIndex) + ".jpg", contoursMat);
+}
+
+struct Vec3bHash {
+	unsigned long operator()(Vec3b const& v) const
+	{
+		size_t x0 = hash<uchar>{}(v[0]);
+		size_t x1 = hash<uchar>{}(v[1]);
+		size_t x2 = hash<uchar>{}(v[2]);
+		return x0 ^ (x1 << 1) ^ (x2 << 2);
+	}
+};
+
 void processDay(Mat daySchedule) {
-	// TODO: implement
+	vector<PixelRange> pixelRanges;
+	PixelRange currPixel;
+	bool first = true;
+
+	uchar r = 234, g = 227, b = 211;
+
+	Mat schedColors = Mat::zeros(daySchedule.rows, 50, CV_8UC3);
+
+	for (uint32_t i = 0; i < daySchedule.rows; i++) {
+		Vec3b *row = daySchedule.ptr<Vec3b>(i);
+
+		// Count occurrences of pixel
+		unordered_map<
+			Vec3b,
+			int,
+			Vec3bHash
+		> pixelOccurrences;
+
+		for (uint32_t j = 0; j < daySchedule.cols; j++) {
+			Vec3b pixel = row[j];
+			if (pixelOccurrences.count(pixel)) {
+				pixelOccurrences[pixel]++;
+			}
+			else {
+				pixelOccurrences[pixel] = 1;
+			}
+		}
+
+		// Find the highest occurring Vec3b
+		int occurrences = -1;
+		Vec3b maxPixel;
+		for (auto &pair : pixelOccurrences) {
+			if (pair.second > occurrences) {
+				maxPixel = pair.first;
+				occurrences = pair.second;
+			}
+		}
+
+		line(schedColors, Point(0, i), Point(50, i), maxPixel);
+		// Append to the current range
+		//if (first) {
+		//	first = false;
+		//	currPixel.range[1]++;
+		//	currPixel.pixel = maxPixel;
+		//}
+		//else if (currPixel.pixel == maxPixel) {
+		//	currPixel.range[1]++;
+		//} else {
+		//	pixelRanges.push_back(currPixel);
+		//	currPixel = { Vec2i(i, i+1), maxPixel };
+		//}
+	}
+	// fencepost
+	//pixelRanges.push_back(currPixel);
+
+	// Construct colorized matrix
+	//Mat schedColors = Mat::zeros(daySchedule.rows, 50, CV_8UC3);
+	/*for (auto range : pixelRanges) {
+		rectangle(schedColors, Point(0, range.range[0]), Point(50, range.range[1]-1), range.pixel);
+	}*/
+	imwrite("../data/schedColors.jpg", schedColors);
 }
 
 int main(int argc, char** argv)
@@ -196,7 +303,7 @@ int main(int argc, char** argv)
 
 	// Cut out the part of the schedule we care about
 	Mat sched = src(Range(leftEdge[3], leftEdge[1]), Range(topEdge[0], topEdge[2]));
-	imwrite("../data/sched.jpg", sched);
+	//imwrite("../data/sched.jpg", sched);
 	
 	// Figure out how tall an hour is
 	Mat timeline = edges(Range(leftEdge[3], leftEdge[1]), Range(0, topEdge[0]));
@@ -204,15 +311,16 @@ int main(int argc, char** argv)
 	cout << "Found hour height: " << hourHeight << endl;
 
 	// Process the schedule day by day; for now assume all 5 days are in the schedule
-	for (size_t i = 0; i < 5; i++) {
+	for (uint32_t i = 0; i < 5; i++) {
 		Mat daySchedule = sched(Range(0, sched.rows), Range(i * sched.cols / 5, (i + 1) * (sched.cols / 5)));
 		imwrite("../data/day" + to_string(i) + ".jpg", daySchedule);
 
 		// TODO: process
-		processDay(daySchedule);
+		processDay2(daySchedule, i, hourHeight);
+		//break;
 	}
 
-	imwrite("../data/output.jpg", output);
+	//imwrite("../data/output.jpg", output);
 	waitKey();
 	return 0;
 }

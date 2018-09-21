2c632f657386708c02b32a5404ddbfce5e675a95
Sarang Joshi <sarangj@msn.com>
2018-09-21 09:35:46 -0700

    Split into windows and linux dirs for now

---
+++ opencv/win/ScheduleCV/OCR.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace std;
+using namespace cv;
+
+void convertToText(vector<Mat> classMats, uint32_t dayIndex);
---
+++ opencv/win/ScheduleCV/Tools.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "opencv2/imgproc/imgproc.hpp"
+
+void drawLine(cv::Mat m, cv::Vec4i line, cv::Scalar color);
